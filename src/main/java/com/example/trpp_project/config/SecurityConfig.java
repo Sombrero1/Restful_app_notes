e29e5af39a6c6cbcff767fdceb272968c8521175
Sombrero1 <vovashevchenko1964@mail.ru>
2021-05-20 20:24:05 +0300

    RESTfull 2.3 добавление самого обычного сваггера

--- src/main/java/com/example/trpp_project/config/SecurityConfig.java
+++ src/main/java/com/example/trpp_project/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST,"/login","/register").permitAll()
+                .antMatchers(HttpMethod.GET,"**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
